de9371e83628ec8233d8970823300cafb24ad4c8
Luqman Ahmed <khawajaluqman98@gmail.com>
2024-01-16 09:26:19 +0200

    Updated portfolio layout for better responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div 
     name="home" 
-    className="h-screen w-full bg-gradient-to-b from-black to-gray-800"
+    className="lg:h-[100vh] w-full bg-gradient-to-b from-black to-gray-800"
     >
         <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4 ">
             <div className="flex flex-col justify-center h-full">
@@ -25,8 +25,8 @@ const Home = () => {
                 <div>
                     <Link 
                      to='portfolio'
-                     smooth
-                     duration={500}
+            smooth
+            duration={500}
                     className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
                         Portfolio
                         <span className="px-4 group-hover:rotate-90 duration-300">
